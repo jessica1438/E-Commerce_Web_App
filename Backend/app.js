350cbaf6746d7943e8ab9b26edbea6d3b273cab6
Jessica Anna James <jessannajames@gmail.com>
2022-11-07 19:26:58 +0300

    added files and wrote the errorhandler class

--- Backend/app.js
+++ Backend/app.js
@@ -1,2 +1,20 @@
 const express = require ("express")
+const mongoose= require("mongoose")
+const dotenv=require("dotenv")
 const app = express()
+const cors = require ("cors")
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//importing all the routes
+const order = require("./routes/OrderRoute")
+const payment = require("./routes/PaymentRoute")
+const product = require("./routes/ProductRoute")
+const user = require("./routes/UserRoute")
+const cart = require( "./routes/CartRoute")
+
+
+
+
