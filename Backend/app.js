ef8be7a04ee1de5586ee4f16824b355666c7fa6a
Jessica Anna James <jessannajames@gmail.com>
2022-11-09 16:04:31 +0300

    connected ot the server

--- Backend/app.js
+++ Backend/app.js
@@ -2,15 +2,17 @@ const express = require ("express")
 const mongoose= require("mongoose")
 const app = express()
 const cors = require ("cors")
+const productss = require("./routes/ProductRoute")
+
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+//app.use(cors())
 
 
-const product = require("./routes/ProductRoute")
 
-app.use("/api/v2",product)
+
+app.use("/api/v2",productss)
 
 module.exports = app
 
