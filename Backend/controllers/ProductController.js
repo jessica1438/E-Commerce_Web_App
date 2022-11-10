da90126faa90cee9ec2e642b99817a43b844e2ad
Jessica Anna James <jessannajames@gmail.com>
2022-11-10 21:22:57 +0300

    controller

--- Backend/controllers/ProductController.js
+++ Backend/controllers/ProductController.js
@@ -18,3 +18,43 @@ const getallProducts = (req,res)=>{
 }
 
 exports.getallProducts = getallProducts
+
+const Product = require("../models/ProductModel.js");
+const ErrorHandler = require("../utils/ErrorHandler.js");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const Features = require("../utils/Features");
+const cloudinary = require("cloudinary");
+
+// create Product --Admin
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
+  let images = [];
+
+  if (typeof req.body.images === "string") {
+    images.push(req.body.images);
+  } else {
+    images = req.body.images;
+  }
+
+  const imagesLinks = [];
+
+  for (let i = 0; i < images.length; i++) {
+    const result = await cloudinary.v2.uploader.upload(images[i], {
+      folder: "products",
+    });
+
+    imagesLinks.push({
+      public_id: result.public_id,
+      url: result.secure_url,
+    });
+  }
+
+  req.body.images = imagesLinks;
+  req.body.user = req.user.id;
+
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
+  });
+});
