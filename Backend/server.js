16724769e595cf052785a638abdf3610e267a4bc
Jessica Anna James <jessannajames@gmail.com>
2022-11-14 21:18:42 +0300

    Section 3 : Added CatchAsyncError, Unhandled rejection and uncaught exception

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,11 @@ const mongoose  = require("mongoose");
 
 require("dotenv").config();
 
+process.on("uncaughtException",(err)=>{
+    console.log(`Error:${err.message}`);
+    console.log(`Shutting down the server for handling uncaught exception`)
+
+})
 
 const port = process.env.PORT ;
 const url = process.env.DATABASE 
@@ -13,4 +18,14 @@ mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=> console.log("Connected to the Database"))
 .then(()=>{app.listen(port)})
 .catch((err) => console.log(err))
+
+
+//unhandled promise rejection
+
+process.on("unhandledRejection",(err)=>{
+    console.log(`Shutting down the server for ${err.message}`);
+    console.log(`Shutting down the server due to unhandled promise rejection`);
+    server.close(()=>{
+        process.exit(1);});
+    });
 
